0d10514de12850ee0f9b9e366c2c1a9e184d5070
Patrick Quinn-Graham <7331+thepatrick@users.noreply.github.com>
2022-11-14 18:04:17 +1100

    Initial Commit

---
+++ ArtProjector/ArtProjectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArtProjectorApp.swift
+//  ArtProjector
+//
+//  Created by Patrick Quinn-Graham on 14/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct ArtProjectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
